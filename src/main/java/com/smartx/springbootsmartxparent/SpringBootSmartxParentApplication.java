c6f403b9107a7e3bd7a1685d57237823cf0c8495
zzhen.liu <zzhen.liu@eventmsimple.com>
2019-04-18 15:20:51 +0800

    第一次提交代码

---
+++ src/main/java/com/smartx/springbootsmartxparent/SpringBootSmartxParentApplication.java
@@ -0,0 +1,13 @@
+package com.smartx.springbootsmartxparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSmartxParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSmartxParentApplication.class, args);
+    }
+
+}
